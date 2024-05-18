a937ecc36689c9b63b484772c769bab502778b71
Tikai7 <halimi.abdelkrim@outlook.fr>
2024-05-18 21:57:54 +0200

    added experiments for autoencoders

--- src/pylamp/utils/functions.py
+++ src/pylamp/utils/functions.py
@@ -1,17 +1,33 @@
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import matplotlib.pyplot as plt
 
-def print_accuracy(model,X_test,y_test):
+def print_accuracy(model,X_test,y_test, print_result=True):
     y_pred = np.argmax(model.forward(X_test),axis=1)
     accuracy = np.sum(y_test == y_pred)/len(y_test)
-    print(f'Accuracy = {accuracy}')
+    if print_result:
+        print(f'Accuracy = {accuracy}')
+    return accuracy
 
 def to_one_hot(y, num_classes):
     return np.eye(num_classes)[y]
 
-def normalize_data(X_train, X_test):
+def normalize_data(X_train, X_test, minmax=False):
+    if minmax:
+        X_train_normalized = (X_train - np.min(X_train, axis=0)) / (np.max(X_train, axis=0) - np.min(X_train, axis=0))
+        X_test_normalized = (X_test - np.min(X_train, axis=0)) / (np.max(X_train, axis=0) - np.min(X_train, axis=0))
+        return X_train_normalized, X_test_normalized
     mean = np.mean(X_train, axis=0)
     std = np.std(X_train, axis=0)
     X_train_normalized = (X_train - mean) / std
     X_test_normalized = (X_test - mean) / std
     return X_train_normalized, X_test_normalized
+
+
+def cluster_kmeans(latent_representations, y_test):
+    kmeans = KMeans(n_clusters=10, random_state=0, n_init=10)
+    clusters = kmeans.fit_predict(latent_representations)
+    ari = adjusted_rand_score(y_test, clusters)
+    print(f"Adjusted Rand Index: {ari:.4f}")
 
--- src/pylamp/utils/plotter.py
+++ src/pylamp/utils/plotter.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.manifold import TSNE
 
 class Display():
     @staticmethod
@@ -28,3 +30,40 @@ class Display():
         plt.title(title2)
         plt.axis('off')
         plt.show()
+
+    @staticmethod
+    def plot_latent_space(X, y, encoder, expand_dims=False):
+        plt.style.use('ggplot')
+        if expand_dims:
+            X = np.expand_dims(X, axis=-1)
+        latent_space = encoder.forward(X)
+        latent_space = latent_space.reshape(latent_space.shape[0], -1)
+        # Reduce the dimensionality to 2D using t-SNE
+        tsne = TSNE(n_components=2, random_state=0)
+        latent_2d = tsne.fit_transform(latent_space)
+        # Assuming y_test contains the labels
+        y_test_np = np.array(y)
+        # Plot the 2D t-SNE projection
+        plt.figure(figsize=(10, 8))
+        for i in range(10):
+            indices = np.where(y_test_np == i)
+            plt.scatter(latent_2d[indices, 0], latent_2d[indices, 1], label=f'Class {i}', alpha=0.6)
+        plt.legend()
+        plt.title("2D t-SNE projection of the latent space")
+        plt.xlabel("t-SNE component 1")
+        plt.ylabel("t-SNE component 2")
+        plt.show()
+
+        return latent_2d
+
+    
+    @staticmethod
+    def plot_reconstruction(model, X, y, n=1, expand_dims=False):
+        plt.style.use('ggplot')
+        plt.figure(figsize=(10,5))
+        for label in np.random.choice(np.unique(y),n):
+            target = X[y == label][0]
+            target = np.expand_dims(target, axis=-1) if expand_dims else target
+            pred = model.forward(np.array([target]))
+            Display.compare_images(target,pred,shape=(16,16),fig_size=(7,3))
+        plt.show() 
